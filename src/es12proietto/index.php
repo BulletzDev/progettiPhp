d5d0551fef19b29b23eed0a9854ab2dec81b0358
BulletzDev <gabri.proietto2007@gmail.com>
2025-11-19 11:47:58 +0100

    started ex12

---
+++ src/es12proietto/index.php
@@ -0,0 +1,40 @@
+<?php 
+    session_start();
+    if (!isset($_SESSION['user_data']) || $_SESSION['user_data']=='ds' ) {
+        header('Location: ./raccoltaDati.php');
+        exit();
+   }
+    if ($_SESSION['user_data']=='cs' ) {
+        header('Location: ./raccoltaContatti.php');
+        exit();
+   }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Riepilogo dati</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <h1 class="text-center my-5">Riepilogo Dati</h1>
+    <div class="container w-50">
+        <ul class="list-group">
+            <li class="list-group-item"><strong>Nome:</strong> <?php echo $_SESSION['user_data']['data']['name']; ?></li>
+            <li class="list-group-item"><strong>Cognome:</strong> <?php echo $_SESSION['user_data']['data']['surname']; ?></li>
+            <li class="list-group-item"><strong>Data di Nascita:</strong>   <?php echo $_SESSION['user_data']['data']['birthdate']; ?></li>
+            <li class="list-group-item"><strong>Indirizzo:</strong> 
+                <?php 
+                    echo $_SESSION['user_data']['data']['address']['street_type'] . ' ' . 
+                         $_SESSION['user_data']['data']['address']['street_name'] . ', ' . 
+                         $_SESSION['user_data']['data']['address']['street_number'];
+                ?></li>
+        </ul>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ src/es12proietto/raccoltaContatti.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['email'])) {
+        $email = isset($_POST['email']) ? trim($_POST['email']) : '';
+        $phone_number = isset($_POST['street_number']) ? trim($_POST['street_number']) : '';
+        $_SESSION['user_data']['contacts'] = [
+            'email' => $email,
+            'phone_number' => $phone_number
+        ];
+        header('Location: ./index.php');
+        exit();
+        $_SESSION['status'] = 'cs';
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inserimento dati</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <form class="row g- w-25 text-center mx-auto my-5" method="post">
+        <div class="col-md-12">
+            <label class="form-label">Email</label>
+            <input type="email" class="form-control" nome="email" required>
+        </div>
+        <div class="input-group my-3">
+            <input type="number" class="form-control" nome="street_number" placeholder="Numero di telefono" required>
+        </div>
+        <div class="col-12">
+            <button type="submit" class="btn btn-primary my-3">Send Data</button>
+        </div>
+    </form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
