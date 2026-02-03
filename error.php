274da30a15f04997eb13dc304ef72ff382fba996
BulletzDev <gabri.proietto2007@gmail.com>
2026-02-03 09:09:13 +0100

    Add initial project files including connection, templates, and customer list

---
+++ error.php
@@ -0,0 +1,3 @@
+<?php
+echo "An error occurred while connecting to the database.";
+?>
---
+++ src/es14proietto/custList.php
@@ -0,0 +1,31 @@
+<?php
+include_once 'connection.php';
+include_once 'headTemplate.php';
+$sql = "SELECT customerName, contactLastName, contactFirstName, phone FROM customers";
+$result = $conn->query($sql);?>
+    <h2>Customer List</h2>
+    <table cellpadding="10" class=" bordered table table-striped mx-auto">
+        <tr>
+            <th>Customer Name</th>
+            <th>Contact Last Name</th>
+            <th>Contact First Name</th>
+            <th>Phone</th>
+        </tr>
+<?php
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        ?>
+        <tr>
+            <td><?php echo htmlspecialchars($row["customerName"]); ?></td>
+            <td><?php echo htmlspecialchars($row["contactLastName"]); ?></td>
+            <td><?php echo htmlspecialchars($row["contactFirstName"]); ?></td>
+            <td><?php echo htmlspecialchars($row["phone"]); ?></td>
+        </tr>
+<?php
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+include_once 'bottomTemplate.php';
+?>
